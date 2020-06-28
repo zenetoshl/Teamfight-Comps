478ba62995254d7095ebda8eba6559e57bb895b4
Jose Neto <zenetoshl@gmail.com>
2020-06-28 00:07:56 -0300

    adicionando assets

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {HexTile} from './src/components/HexTile'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to working your app!</Text>
+      <HexTile />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#aff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/HexTile/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+
+import { AppRegistry, Text, StyleSheet, Image, ImageBackground } from "react-native";
+
+export const HexTile = () => (
+    //<Image source={require("../../assets/champions/ahri.png")} />
+    <Text>dsa</Text>
+);
